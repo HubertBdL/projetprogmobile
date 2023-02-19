7a0210760cd1a4425baee8fab19bce149b3452e7
HubertBdL <hbiard33@gmail.com>
2023-02-19 15:37:33 +0100

    Version-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
  Future <void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
-
-
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
